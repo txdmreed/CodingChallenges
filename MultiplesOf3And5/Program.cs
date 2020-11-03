7120f3fbac7d689a28bd1f4cc2a339b4baa31ca8
txdmreed <txdmreed@outlook.com>
2020-11-02 21:07:43 -0600

    Initial Commit, first multiples challenge

---
+++ MultiplesOf3And5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplesOf3And5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int m = 0; m < 1000; m++)
+            {
+                if ((m % 3 == 0) || (m % 5 == 0))
+                {
+                    sum = sum + m;
+                }
+            }
+
+            Console.WriteLine(sum);
+
+
+        }//
+    }//
+}//
