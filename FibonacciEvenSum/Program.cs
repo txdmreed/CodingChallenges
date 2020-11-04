8f0c4271fa42234e9de3a9935107256ca63ad353
txdmreed <txdmreed@outlook.com>
2020-11-03 22:33:08 -0600

    Coding Challenge two

---
+++ FibonacciEvenSum/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciEvenSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long num1 = 1;
+            long num2 = 1;
+            long result = 0;
+            long summed = 0;
+
+            while (result < 4000000)
+            {
+                if ((result % 2) == 0)
+                {
+                    summed += result;
+                }
+
+                result = num1 + num2;
+                num2 = num1;
+                num1 = result;
+            }
+
+            Console.WriteLine(summed);
+
+        }//
+    }//
+}//
